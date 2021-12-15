313474c9e39acca3476bc3caf426cfe50d68f624
naradanarya <52026986+naradanarya@users.noreply.github.com>
2021-12-15 12:54:48 +0700

    add feature

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,43 @@
+import "./featured.scss";
+import { PlayArrow, InfoOutlined } from "@material-ui/icons";
+
+export default function Featured({ type }) {
+  return (
+    <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="comedy">Comedy</option>
+            <option value="romance">Romance</option>
+          </select>
+        </div>
+      )}
+      <img src="https://wallpaperaccess.com/full/1188141.jpg" alt="" />
+      <div className="info">
+        <span className="desc">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,15 +1,12 @@
 import Navbar from "../components/navbar/Navbar";
+import Featured from "../components/featured/Featured";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img
-        width="100%"
-        src="https://wallpapercave.com/wp/wp6434337.jpg"
-        alt=""
-      />
+      <Featured/>
     </div>
   );
 };
