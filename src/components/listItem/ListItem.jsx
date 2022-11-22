f3d7a15a598f15da60d64707198e5040403f3d17
naradanarya <52026986+naradanarya@users.noreply.github.com>
2022-11-22 19:42:54 +0700

    update client

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,50 @@
-import "./listItem.scss"
+import {
+  Add,
+  PlayArrow,
+  ThumbDownOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
+import { useState } from "react";
+import "./listItem.scss";
 
-export default function ListItem() {
-    return (
-        <div className="listItem">
-            item
-        </div>
-    )
+export default function ListItem({ index }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer =
+    "https://drive.google.com/uc?export=download&id=1hHAjQR-Vz4-rzU8_14AxXMT8BNJkfTR2";
+  return (
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img
+        src="https://lepasjenuh.com/wp-content/uploads/2021/01/horimiya-anime-1252274.jpeg"
+        alt=""
+      />
+
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop type="video/mp4"></video>
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon"/>
+              <Add className="icon"/>
+              <ThumbUpAltOutlined className="icon"/>
+              <ThumbDownOutlined className="icon"/>
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>2019</span>
+            </div>
+            <div className="desc">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+            </div>
+            <div className="genre">Romace</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,16 @@
 import Navbar from "../components/navbar/Navbar";
 import Featured from "../components/featured/Featured";
+import List from "../components/list/List"
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      <Navbar />
+      <Navbar/>
       <Featured/>
+      <List/>
+      <List/>
+      <List/>
     </div>
   );
 };
